dfbaf394591eb39a1c53a364e763c8827822a62d
Gustav <gustav-git@pm.me>
2024-03-10 14:48:10 +0100

    Add link to club from player profile

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 import supabase
+from supabase.lib.client_options import ClientOptions
 
 from app.badminton_player import api
 
@@ -9,4 +10,5 @@ badminton_player_client = api.Client()
 supabase_client = supabase.create_client(
     supabase_url=os.getenv("SUPABASE_URL"),
     supabase_key=os.getenv("SUPABASE_KEY"),
+    options=ClientOptions(postgrest_client_timeout=10000),
 )
